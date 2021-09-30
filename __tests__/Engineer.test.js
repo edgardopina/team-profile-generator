b1648fe1c174173b0cdc2d8a64c15b6810bc009a
edgardopina <edgardopina@yahoo.com>
2021-09-29 23:50:36 -0600

    Created test case for Engineer object - FAILED.

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,14 @@
+const Engineer = require('../lib/Engineer');
+
+test('create Engineer object', () => {
+	const engineer = new Engineer('Eng Edgardo Pina', 999777, 'eng-edgardopina@yahoo.com', 'edgardopina');
+	expect(engineer.name).toBe('Eng Edgardo Pina');
+	expect(engineer.id).toBe(999777);
+   expect(engineer.email).toBe('eng-edgardopina@yahoo.com');
+   expect(engineer.github).toBe('edgardopina');
+	expect(engineer.getName()).toBe('Eng Edgardo Pina');
+	expect(engineer.getId()).toBe(999777);
+   expect(engineer.getEmail()).toBe('eng-edgardopina@yahoo.com');
+   expect(engineer.getGithub()).toBe('edgardopina');
+	expect(engineer.getRole()).toBe('Engineer');
+});
