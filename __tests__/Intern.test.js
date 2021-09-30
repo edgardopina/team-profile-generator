7ffd5a66be5dfba80c90c1046604d5bc0e78106a
edgardopina <edgardopina@yahoo.com>
2021-09-30 00:03:01 -0600

    Created test case for Intern object - Test case: FAILED.

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,14 @@
+const Intern = require('../lib/Intern');
+
+test('create Intern object', () => {
+	const intern = new Intern('Int Edgardo Pina', 111777, 'int-edgardopina@yahoo.com', 'UofU');
+	expect(intern.name).toBe('Int Edgardo Pina');
+	expect(intern.id).toBe(111777);
+	expect(intern.email).toBe('int-edgardopina@yahoo.com');
+	expect(intern.school).toBe('UofU');
+	expect(intern.getName()).toBe('Int Edgardo Pina');
+	expect(intern.getId()).toBe(111777);
+	expect(intern.getEmail()).toBe('int-edgardopina@yahoo.com');
+	expect(intern.getGithub()).toBe('edgardopina');
+	expect(intern.getRole()).toBe('Intern');
+});
